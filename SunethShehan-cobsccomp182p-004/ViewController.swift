470628a7455cc762160ddb4c07ce8082e6845aa9
Suneth <dse16137@student.nibm.lk>
2020-02-03 15:44:36 +0530

    Initial commit

---
+++ SunethShehan-cobsccomp182p-004/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SunethShehan-cobsccomp182p-004
+//
+//  Created by Suneth on 2/3/20.
+//  Copyright © 2020 Suneth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
