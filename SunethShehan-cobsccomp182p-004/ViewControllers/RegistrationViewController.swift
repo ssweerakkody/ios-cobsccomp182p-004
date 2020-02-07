9283aea76af9227f21a22cb33e65075fe8655531
Suneth <dse16137@student.nibm.lk>
2020-02-07 12:55:01 +0530

    profile pic and additional details are added

--- SunethShehan-cobsccomp182p-004/ViewControllers/RegistrationViewController.swift
+++ SunethShehan-cobsccomp182p-004/ViewControllers/RegistrationViewController.swift
@@ -56,8 +56,6 @@ class RegistrationViewController: UIViewController{
         
         //self.ref.child("df").child("articles") .setValue(["username": "testusername2"])
         
-        self.ref.child("Users").childByAutoId().setValue(["FirstName":txtFName.text!,"LastName":txtLName.text!,"Email":txtEmail.text!,"Password":txtPassword.text!])
-        
         //create the user in authentication
         
         Auth.auth().createUser(withEmail: txtEmail.text!, password: txtPassword.text!) { authResult, error in
@@ -77,6 +75,126 @@ class RegistrationViewController: UIViewController{
             }
             
         }
+        
+//        self.ref.child("Users").childByAutoId().setValue(["FirstName":txtFName.text!,"LastName":txtLName.text!,"Email":txtEmail.text!,"Password":txtPassword.text!])
+
+        
+        guard let FirstName = txtFName.text, !FirstName.isEmpty else {
+            
+            showAlert(title: "Check input",message: "FirstName cannot be empty")
+            return
+        }
+        
+        guard let LastName = txtLName.text, !LastName.isEmpty else {
+            
+            showAlert(title: "Check input",message: "LastName cannot be empty")
+            return
+        }
+        
+        guard let Email = txtEmail.text, !Email.isEmpty else {
+            
+            showAlert(title: "Check input",message: "Email cannot be empty")
+            return
+        }
+        
+        guard let Password = txtPassword.text, !Password.isEmpty else {
+            
+            showAlert(title: "Check input",message: "Password cannot be empty")
+            return
+        }
+        
+        guard let ConfirmPassword = txtConfirmPassword.text, !ConfirmPassword.isEmpty else {
+            
+            showAlert(title: "Check input",message: "Confirm Password cannot be empty")
+            return
+        }
+        
+        guard let MobileNo = txtMobileNo.text, !MobileNo.isEmpty else {
+            
+            showAlert(title: "Check input",message: "MobileNo cannot be empty")
+            return
+        }
+        
+        //Make this optional or alternation
+        guard let image = imgProPicture.image,
+            let imgData = image.jpegData(compressionQuality: 1.0) else {
+                
+                showAlert(title: "Check input",message: "Profile Picture must be selected")
+                return
+        }
+        
+        let FBProfileUrl = txtFBProfileUrl.text!
+        
+        let alert = UIAlertController(title: nil, message: "Saving ", preferredStyle: .alert)
+        
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.gray
+        loadingIndicator.startAnimating();
+        
+        alert.view.addSubview(loadingIndicator)
+        present(alert, animated: true, completion: nil)
+        
+        
+        let imageName = UUID().uuidString
+        
+        let reference = Storage.storage().reference().child("UserProfileImages").child(imageName)
+        
+        
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        
+        reference.putData(imgData, metadata: metaData) { (meta, err) in
+            if let err = err {
+                alert.dismiss(animated: false, completion: nil)
+                self.showAlert(title: "Eror",message: "Error uploading image: \(err.localizedDescription)")
+                return
+            }
+            
+            reference.downloadURL { (url, err) in
+                if let err = err {
+                    alert.dismiss(animated: false, completion: nil)
+                    self.showAlert(title: "Eror",message: "Error fetching url: \(err.localizedDescription)")
+                    return
+                }
+                
+                guard let url = url else {
+                    alert.dismiss(animated: false, completion: nil)
+                    self.showAlert(title: "Eror",message: "Error getting url")
+                    return
+                }
+                
+                let imgUrl = url.absoluteString
+                
+                
+                let dbRef = Database.database().reference().child("Users").childByAutoId()
+                
+                
+                let data = [
+                    "FirstName" : FirstName,
+                    "LastName" : LastName,
+                    "Email":Email,
+                    "MobileNo": MobileNo,
+                    "ProfileImageUrl" : imgUrl,
+                    "FBProfileUrl":FBProfileUrl
+                    ]
+                
+                dbRef.setValue(data, withCompletionBlock: { ( err , dbRef) in
+                    if let err = err {
+                        self.showAlert(title: "Eror",message: "Error uploading data: \(err.localizedDescription)")
+                        return
+                    }
+                    alert.dismiss(animated: false, completion: nil)
+                    
+                    self.showAlert(title: "Success", message: "User Saved !")
+                    
+                    
+                })
+                
+            }
+        }
+        
         
     }
     
