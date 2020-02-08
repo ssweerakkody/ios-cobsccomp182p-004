82b8572aaaa0dcc1c71415e8deb4ccd4adb7ebef
Suneth <dse16137@student.nibm.lk>
2020-02-08 17:27:29 +0530

    Tabbed controllers are added

---
+++ SunethShehan-cobsccomp182p-004/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SampleTabApp
+//
+//  Created by Suneth on 2/7/20.
+//  Copyright © 2020 Suneth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
