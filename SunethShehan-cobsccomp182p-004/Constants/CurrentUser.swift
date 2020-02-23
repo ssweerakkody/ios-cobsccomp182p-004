8fd15c315e78cb7b71f97c35362043abe14a9c19
Suneth <dse16137@student.nibm.lk>
2020-02-23 12:44:44 +0530

    Authentication client is added

---
+++ SunethShehan-cobsccomp182p-004/Constants/CurrentUser.swift
@@ -0,0 +1,15 @@
+//
+//  CurrentUser.swift
+//  SunethShehan-cobsccomp182p-004
+//
+//  Created by Suneth on 2/23/20.
+//  Copyright © 2020 Suneth. All rights reserved.
+//
+
+import Foundation
+
+class CurrentUser{
+    
+//    var user : User()
+    
+}
---
+++ SunethShehan-cobsccomp182p-004/Helpers/FAuthClient.swift
@@ -0,0 +1,19 @@
+//
+//  FAuthClient.swift
+//  SunethShehan-cobsccomp182p-004
+//
+//  Created by Suneth on 2/23/20.
+//  Copyright © 2020 Suneth. All rights reserved.
+//
+
+import UIKit
+
+class FAuthClient {
+    
+    static func createNewUser(email:String,password:String,viewController:UIViewController){
+        
+        
+        
+    }
+    
+}
