af2cb620982eb0a27499b6da316dee8e7f5da4ed
Suneth <suneth@Suneths-iMac.local>
2020-02-29 08:50:24 +0530

    commmets view is added

---
+++ SunethShehan-cobsccomp182p-004/CustomTableCells/CommentsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CommentsTableViewCell.swift
+//  SunethShehan-cobsccomp182p-004
+//
+//  Created by Suneth on 2/29/20.
+//  Copyright © 2020 Suneth. All rights reserved.
+//
+
+import UIKit
+
+
+class CommentsTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var lblCommentText: UILabel!
+    
+    @IBOutlet weak var lblCreatedBy: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // addStylesToCell()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+
+}
---
+++ SunethShehan-cobsccomp182p-004/ViewControllers/Events/Comments/TableViews/CommentsTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CommentsTableViewController.swift
+//  SunethShehan-cobsccomp182p-004
+//
+//  Created by Suneth on 2/29/20.
+//  Copyright © 2020 Suneth. All rights reserved.
+//
+
+import UIKit
+
+
+class CommentsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "backimage.jpg")!)
+        
+        
+        
+        
+    }
+    
+}
