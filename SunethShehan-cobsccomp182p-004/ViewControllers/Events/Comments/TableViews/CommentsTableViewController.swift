ae4bafc9a30feea072f6dcea3957035f097312b4
Suneth <suneth@Suneths-iMac.local>
2020-02-29 09:57:25 +0530

    comment table view methods are added

--- SunethShehan-cobsccomp182p-004/ViewControllers/Events/Comments/TableViews/CommentsTableViewController.swift
+++ SunethShehan-cobsccomp182p-004/ViewControllers/Events/Comments/TableViews/CommentsTableViewController.swift
@@ -11,14 +11,81 @@ import UIKit
 
 class CommentsTableViewController: UITableViewController {
     
+    var Comments = [String](){
+        didSet{
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "backimage.jpg")!)
         
+        Comments.append("Comment one")
+        Comments.append("Comment two")
         
         
         
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Comments.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell",for: indexPath) as! CommentsTableViewCell
+        
+        cell.lblCommentText.text = Comments[indexPath.row]
+        
+        
+        
+        return cell
+    }
+    
+    
+    @IBAction func AddComment(_ sender: Any) {
+        
+        let ac = UIAlertController(title: "Enter Comment", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+            return
+        }))
+        
+        ac.addAction(UIAlertAction(title: "Post", style: .default) { [unowned ac] _ in
+            let answer = ac.textFields![0]
+            self.Comments.append(answer.text!)
+            self.tableView.reloadData()
+            
+        })
+        
+        
+        present(ac, animated: true)
+        
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
+            // delete item at indexPath
+        }
+        
+        let share = UITableViewRowAction(style: .normal, title: "Edit") { (action, indexPath) in
+            // share item at indexPath
+        }
+        
+        share.backgroundColor = UIColor.blue
+        
+        return [delete, share]
     }
     
 }
