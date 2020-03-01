f836352dc05ad959a20576622b6070bae8c09329
ssweerakkody <cobsccomp182p-004@student.nibm.lk>
2020-03-01 17:27:45 +0530

    testing methods are added

--- SunethShehan-cobsccomp182p-004Tests/UserAuthenticationTest.swift
+++ SunethShehan-cobsccomp182p-004Tests/UserAuthenticationTest.swift
@@ -13,7 +13,6 @@ import UIKit
 
 class UserAuthenticationTest: XCTestCase {
 
-     var firebaseAuth = Auth.auth()
     override func setUp() {
        
     }
@@ -22,20 +21,31 @@ class UserAuthenticationTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func validateUserInputs(){
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testEmailValidity(){
         
-        let loginVC = LoginViewController()
-        loginVC.txtEmail.text = "dummy@mail.com"
-        loginVC.txtPassword.text = "123456"
+        let txtEmail = UITextField()
+        txtEmail.text = "dummy@mail.com"
+        XCTAssertTrue(FormValidation.isValidEmail(txtEmail.text!, presentingVC: UIViewController()), "Valid Email")
+    }
+    
+    func testPasswordValidity(){
         
-        XCTAssertTrue(loginVC.validateInputs(), "Valid Inputs")
+        let txtPassword = UITextField()
+        txtPassword.text = "123456"
         
-        
+        XCTAssertTrue(FormValidation.isValidEmail(txtPassword.text!, presentingVC: UIViewController()), "Valid Password")
     }
     
     func testUserAuthentication() {
         
-
+        
         FAuthClient.signInUser(email: "dummyuser@mail.com", password: "123456", presentingVC: UIViewController()) { (user) in
             if(user != nil)
             {
@@ -45,16 +55,9 @@ class UserAuthenticationTest: XCTestCase {
             {
                 XCTFail("Login Failed")
             }
-        
+            
         }
         
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
